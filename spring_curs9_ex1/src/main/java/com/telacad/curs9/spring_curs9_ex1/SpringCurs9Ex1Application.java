e00e66be4b24f4b487df11b1591f8618a715f126
PaulMir17 <miricapaul17@gmail.com>
2024-07-18 14:46:18 +0300

    Add files via upload

---
+++ spring_curs9_ex1/src/main/java/com/telacad/curs9/spring_curs9_ex1/SpringCurs9Ex1Application.java
@@ -0,0 +1,13 @@
+package com.telacad.curs9.spring_curs9_ex1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCurs9Ex1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCurs9Ex1Application.class, args);
+	}
+
+}
---
+++ spring_curs9_ex1/src/test/java/com/telacad/curs9/spring_curs9_ex1/SpringCurs9Ex1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.telacad.curs9.spring_curs9_ex1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCurs9Ex1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
